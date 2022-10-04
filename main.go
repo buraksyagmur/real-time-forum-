f92d5838555ba0d18d81d5a3af78fe5c1735e5f9
pkdc <pau.darco371@gmail.com>
2022-10-04 18:44:00 +0100

    rendered basic login form

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"forum/forum"
 	"log"
 	"net/http"
 	"os/exec"
+
+	"forum/forum"
 )
 
 func main() {
@@ -17,13 +18,13 @@ func main() {
 
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 	http.HandleFunc("/", forum.HomeHandler)
-	http.HandleFunc("/login", forum.LoginHandler)
-	http.HandleFunc("/register", forum.RegisterHandler)
-	http.HandleFunc("/logout", forum.LogoutHandler)
-	http.HandleFunc("/postpage", forum.PostPageHandler)
-	http.HandleFunc("/notifications", forum.NotiPageHandler)
-	http.HandleFunc("/activity", forum.ActivityPageHandler)
-	// http.HandleFunc("/delete", forum.DeleteHandler)
+	// http.HandleFunc("/login", forum.LoginHandler)
+	// http.HandleFunc("/register", forum.RegisterHandler)
+	// http.HandleFunc("/logout", forum.LogoutHandler)
+	// http.HandleFunc("/postpage", forum.PostPageHandler)
+	// http.HandleFunc("/notifications", forum.NotiPageHandler)
+	// http.HandleFunc("/activity", forum.ActivityPageHandler)
+	// // http.HandleFunc("/delete", forum.DeleteHandler)
 	fmt.Println("Starting server at port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
