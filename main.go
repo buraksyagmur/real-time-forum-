3975cc8f18332b9cbf153907f2a2c6ce316993a4
pkdc <pau.darco371@gmail.com>
2022-10-04 17:24:03 +0100

    initial commit, created tables

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"forum/forum"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+func main() {
+	forum.InitDB()
+	// forum.ClearUsers()
+	// forum.ClearPosts()
+	// forum.ClearComments()
+	exec.Command("xdg-open", "http://localhost:8080/").Start()
+
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.HandleFunc("/", forum.HomeHandler)
+	http.HandleFunc("/login", forum.LoginHandler)
+	http.HandleFunc("/register", forum.RegisterHandler)
+	http.HandleFunc("/logout", forum.LogoutHandler)
+	http.HandleFunc("/postpage", forum.PostPageHandler)
+	http.HandleFunc("/notifications", forum.NotiPageHandler)
+	http.HandleFunc("/activity", forum.ActivityPageHandler)
+	// http.HandleFunc("/delete", forum.DeleteHandler)
+	fmt.Println("Starting server at port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
