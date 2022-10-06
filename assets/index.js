f7ab9e4cfb01bbe5c3248a87deafde72295434dc
brksygmr <brksygmr@outlook.com>
2022-10-06 16:39:50 +0100

    regForm

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,14 @@
-import regForm from "./reglog.js";
-const regFormDiv = document.querySelector("#reg-form-div");
-regFormDiv.append(regForm);
+import RegisterForm from "./reg.js";
+import loginForm from "./login.js";
 
+const formDiv = document.querySelector(".form-div");
+const loginBut = document.querySelector(".login-btn");
+const regBut = document.querySelector(".reg-btn");
+
+// formDiv.append(loginForm);
+regBut.append(RegisterForm)
+loginBut.append(loginForm)
+
+document.addEventListener("DOMContentLoaded", function() {
+    formDiv.classList.add("show");
+});
