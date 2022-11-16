1ffbbbb89119af71f6568fbb49b87dd8b4fa35ae
brksygmr <brksygmr@outlook.com>
2022-11-16 18:19:06 +0000

    started to chat

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     console.log("JS attempt to connect to chat");
     chatSocket.onopen = () => console.log("chat connected");
     chatSocket.onclose = () => console.log("Bye chat");
-    chatSocket.onerror = (err) => console.log("chat ws Error!");
+    chatSocket.onerror = (err) => console.log("chat ws Error!",err);
     chatSocket.onmessage = (msg) => {
         const resp = JSON.parse(msg.data);
         console.log({resp});
@@ -32,12 +32,21 @@ document.addEventListener("DOMContentLoaded", function(e) {
                 nickname.textContent = `${uNickname}`;
                 uList.append(nickname);
             }
-        } else if (resp.label === "chat") {
+        } else if (resp.label === "message") {
             console.log(resp.content);
+            MessageHandler
         }
     }
 })
-
+const MessageHandler = function (e) {
+    e.preventDefault();
+    let payloadObj = {}
+    payloadObj["label"] = "message";
+    payloadObj["content"] = "input"
+    payloadObj["userID"] = 2
+    payloadObj["contactID"] = 1
+    chatSocket.send(JSON.stringify(payloadObj));
+};
 // const chatBox = document.createElement("form");
 // chatBox.id = "chat-form"
 
--- forum/chat.go
+++ forum/chat.go
@@ -1,22 +1,40 @@
 package forum
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+type Clients struct {
+	conn   *websocket.Conn
+	userID int
+}
+
 type WsChatResponse struct {
-	Label   string `json:"label"`
-	Content string `json:"content"`
+	Label     string `json:"label"`
+	Content   string `json:"content"`
+	UserID    string `json:"userID"`
+	ContactID string `json:"contactID"`
+}
+
+type MessageArray struct {
+	Index int           `json:"index"`
+	Msg   WsChatPayload `json:"msgInfo"`
 }
 
 type WsChatPayload struct {
-	Label      string `json:"label"`
-	Content    string `json:"content"`
-	ReceiverId int    `json:"receiver_id"` // for chat
+	Label       string `json:"label"`
+	Content     string `json:"content"`
+	ReceiverId  int    `json:"receiver_id"` // for chat
+	SenderId    int    `json:"sender_id"`
+	MessageTime string `json:"message_time"`
+	Noti        bool   `json:"noti"`
+	Right       bool   `json:"right_side"`
 }
 
 var chatPayloadChan = make(chan WsChatPayload)
@@ -42,7 +60,69 @@ func readChatPayloadFromWs(conn *websocket.Conn) {
 		err := conn.ReadJSON(&chatPayload)
 		if err == nil {
 			fmt.Printf("Sending chatPayload thru chan: %v\n", chatPayload)
+			listeningChat(conn, chatPayload)
 			chatPayloadChan <- chatPayload
+
 		}
 	}
+}
+
+func listeningChat(conn *websocket.Conn, msg WsChatPayload) {
+	var chatResponse WsChatResponse
+	defer func() {
+		fmt.Println("chat Ws Conn Closed")
+	}()
+	for {
+		if msg.Label == "message" {
+			var pureMsg WsChatPayload
+			json.Unmarshal([]byte(msg.Content), &pureMsg)
+			processMsg(pureMsg)
+			fmt.Printf("payload received: %v\n", msg)
+		} else if msg.Label == "openChatBox" {
+			chatResponse.Label = "openChatBox"
+			chatResponse.Content = displayChatInfo(msg.SenderId, msg.ReceiverId)
+			// chatResponse.UserID, chatResponse.ContactID = msg.SenderId, msg.ReceiverId
+			fmt.Printf("payload received: %v\n", msg)
+		}
+	}
+}
+
+func processMsg(msg WsChatPayload) {
+	rows, err := db.Prepare("INSERT INTO messages(senderID,receiverID,messageTime,content,seen) VALUES(?,?,?,?,?);")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	rows.Exec(msg.SenderId, msg.ReceiverId, time.Now(), msg.Content, false)
+	fmt.Println("msg saved successfully")
+}
+
+func displayChatInfo(sendID, recID int) string {
+	var allMsg MessageArray
+	var arrMsgArray []MessageArray
+	counter := 0
+	rows, err := db.Query(`SELECT senderID,receiverID,messageTime, content, seen FROM messages 
+	WHERE (senderID = ? AND receiverID = ?) or (senderID = ? AND receiverID = ?)`, sendID, recID, recID, sendID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var oneMsg WsChatPayload
+		var msgTime time.Time
+		rows.Scan(&(oneMsg.SenderId), &(oneMsg.ReceiverId), &msgTime, &(oneMsg.Content), &(oneMsg.Noti))
+		oneMsg.MessageTime = msgTime.String()
+		if oneMsg.SenderId == sendID {
+			oneMsg.Right = true
+		}
+		allMsg.Index = counter
+		allMsg.Msg = oneMsg
+		counter++
+		arrMsgArray = append(arrMsgArray, allMsg)
+	}
+	jsonF, err := json.Marshal(arrMsgArray)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(jsonF)
 }
